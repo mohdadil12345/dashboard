eac2c3adbfa215783c6a3280c17a39878de1dbcf
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-03-23 22:43:50 +0530

    filter and sorting done

--- dashboard/src/app/app.component.ts
+++ dashboard/src/app/app.component.ts
@@ -8,6 +8,16 @@ import { ProdType } from './prod-type';
 })
 export class AppComponent {
 
+  prod_Data: ProdType[] = [];
+
+  prd_name: string = '';
+  
+  asc: boolean = true;
+
+
+  constructor() {
+    this.prod_Data = this.products; 
+  }
 
 
    products:  ProdType[] =  [
@@ -16,8 +26,29 @@ export class AppComponent {
     { id: 3, productName: 'Headphones', price: 150, category: 'Electronics' },
     { id: 4, productName: 'Desk', price: 300, category: 'Furniture' },
     { id: 5, productName: 'Chair', price: 100, category: 'Furniture' },
+    { id: 6, productName: 'Headphones', price: 100, category: 'Electronics' },
+    { id: 7, productName: 'Chair', price: 100, category: 'Furniture' },
+    { id: 8, productName: 'Smartphone', price: 800, category: 'Electronics' },
   ];
-  
 
+
+  // sorting
+
+  sort_price() {
+    this.asc = !this.asc;
+    this.prod_Data.sort((a, b) => {
+      return this.asc ? a.price - b.price : b.price - a.price;
+    });
+  }
+
+
+  // filter
+  
+  filBy_name() {
+    this.prod_Data = this.products.filter(ele =>
+      ele.productName.toLowerCase().includes(this.prd_name.toLowerCase())
+    );
+  }
+  
 
 }
